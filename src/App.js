f5fa9bcc549436739c6f7ff5227a8bb7002ad80d
Maryna-Zeinalova <103852773+Maryna-Zeinalova@users.noreply.github.com>
2022-10-27 21:03:15 +0300

    Created react-weather-app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Weather App</h1>
+      <p>This site was created by Maryna Zeinalova and is open-sourced</p>
     </div>
   );
 }
